05ea172c707045092a5ffae77807a4b60eade75b
Vera Dobrodeeva <dobro.vera@gmail.com>
2023-11-30 16:31:59 +0300

    railway deeploy, allowed host 0.0.0.0

--- simple_solutions_test/settings.py
+++ simple_solutions_test/settings.py
@@ -26,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*z7k3tfdd3ir&h+5o%-zkx*n8037kiapu-4%i2%@+_6zi7g*nq'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
